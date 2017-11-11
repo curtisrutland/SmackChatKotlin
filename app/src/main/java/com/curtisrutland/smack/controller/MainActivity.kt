faa86341d56fcfa7390c8a724a462275e82febbf
Curtis Rutland <curtisrutland@gmail.com>
2017-11-11 13:34:34 -0600

    refactored activity into controller package

--- app/src/main/java/com/curtisrutland/smack/controller/MainActivity.kt
+++ app/src/main/java/com/curtisrutland/smack/controller/MainActivity.kt
@@ -1,14 +1,11 @@
-package com.curtisrutland.smack
+package com.curtisrutland.smack.controller
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import android.view.View
+import com.curtisrutland.smack.R
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
